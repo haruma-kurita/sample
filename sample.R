fc30cc8a87dfb00e6a8d3ff7c0325cd6da914d10
Haruma Kurita <hk348@duke.edu>
2025-10-21 12:28:55 -0400

    Penguins Rule the World

--- sample.R
+++ sample.R
@@ -1,1 +1,10 @@
 "Welcome to my Github"
+
+library(palmerpenguins)
+library(ggplot2)
+
+ggplot(penguins, aes(x = bill_length_mm, y = bill_depth_mm, color = species)) +
+  geom_point() +
+  theme_bw()
+
+mean(penguins$bill_depth_mm, na.rm = TRUE)
